ddcd754405701ed959174a0c73c598e244ac7678
wesle <wesleynederpel43@gmail.com>
2024-06-17 10:13:27 +0200

    spritesheets + nps.js

--- src/js/resources.js
+++ src/js/resources.js
@@ -4,6 +4,11 @@ import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 const Resources = {
     Fish: new ImageSource('images/fish.png'),
     StartScreen: new ImageSource('images/startscreen.png'),
+    Player: new ImageSource('images/mees-mystkeeper.png'),
+    Wout: new ImageSource('images/boswachter-wout.png'),
+    Dirk: new ImageSource('images/dirk-zeebries.png'),
+    Elara: new ImageSource('images/elara-mystveil.png'),
+    Lithorock: new ImageSource('images/lithorock.png')
 }
 
 const ResourceLoader = new Loader()
