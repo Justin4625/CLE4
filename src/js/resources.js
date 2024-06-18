d21db4996b60856de624a3cb717bace03f22083b
wesle <wesleynederpel43@gmail.com>
2024-06-18 13:37:00 +0200

    PlayerF in resources en Images

--- src/js/resources.js
+++ src/js/resources.js
@@ -11,6 +11,7 @@ const Resources = {
     Youdidit: new ImageSource('images/youdidit.png'),
     Map: new TiledResource('map/tilemap.tmx'),
     PlayerM: new ImageSource('images/mees-mystkeeper.png'),
+    PlayerF: new ImageSource('images/alice-mystkeeper.png'),
     Wout: new ImageSource('images/boswachter-wout.png'),
     Dirk: new ImageSource('images/dirk-zeebries.png'),
     Elara: new ImageSource('images/elara-mystveil.png'),
