ebcf06defbbb72c13f472b102d534d2e5fc70d8a
meesh <meeshoff2006@gmail.com>
2024-06-19 12:40:25 +0200

    Map changes

--- src/js/map.js
+++ src/js/map.js
@@ -20,7 +20,7 @@ export class Map extends Scene {
     }
 
     onInitialize(engine) {
-        console.log("start de game!");
+        console.log("start de game! Blabla");
         // const tiledMap = new TiledResource('map/tilemap.tmx');
         // tiledMap.addToScene(this);
 
